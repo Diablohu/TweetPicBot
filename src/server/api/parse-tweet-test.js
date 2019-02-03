8d23cbfc1abd08c219573b1dd1f137a3f7642b65
Diablohu <Diablohu@users.noreply.github.com>
2019-02-04 02:04:13 +0800

    api/parse-tweet

--- src/server/api/parse-tweet-test.js
+++ src/server/api/parse-tweet-test.js
@@ -1,8 +1,16 @@
 const run = async () => {
-    const result = await require('./parse-tweet.js')('ogatatei', '1091511648244850689', {
+    const options = {
         headless: false,
         proxy: 'socks5'
-    })
-    console.log(result)
+    }
+    console.log(await require('./parse-tweet.js')(
+        'ogatatei', '1091511648244850689', options
+    ))
+    console.log(await require('./parse-tweet.js')(
+        'yochris72', '1092055434234458112', options
+    ))
+    console.log(await require('./parse-tweet.js')(
+        'Diablohu', '1090874382841307136', options
+    ))
 }
 run()
